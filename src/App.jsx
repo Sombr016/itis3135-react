a0ca4e799b8790d8b33cd0b33ab30dd63289aa40
Jacob K <jwkilcrease2004@gmail.com>
2025-11-10 18:24:21 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,7 @@ function App() {
       {/* these are empty tags called Fragments */}
       <h2>Home</h2>
 
-        <p>This is all the content for my ITIS 3135 class</p>
-
-        <p>Insert from later projects here</p>
+        <p>This is all the content for my ITIS 3135 class.</p>
     </>
   );
 }
