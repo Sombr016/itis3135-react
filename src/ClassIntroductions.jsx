0a57bc3b0372bfc6b1c3bbcc844c811fce74bb99
Jacob K <jwkilcrease2004@gmail.com>
2025-12-02 13:51:57 -0500

    more intro filters

--- src/ClassIntroductions.jsx
+++ src/ClassIntroductions.jsx
@@ -8,9 +8,16 @@ export default function ClassIntroduction() {
     const [displayName, setDisplayName] = useState(true);
     const [displayMascot, setDisplayMascot] = useState(true);
     const [displayImage, setDisplayImage] = useState(true);
+    const [displayPersonalStatement, setDisplayPersonalStatement] = useState(true);
+    const [displayBackgrounds, setDisplayBackgrounds] = useState(true);
+    const [displayClasses, setDisplayClasses] = useState(true);
+    const [displayExtraInformation, setDisplayExtraInformation] = useState(true);
+    const [displayQuote, setDisplayQuote] = useState(true);
+    const [displayLinks, setDisplayLinks] = useState(true);
     const [showSlideShow, setShowSlideShow] = useState(false);
-    const [indexOfSlideShow, setIndexOfSlideShow]  = useState(0);
+    const [indexOfSlideShow, setIndexOfSlideShow] = useState(0);
 
+    {/* Getting information from API*/ }
     useEffect(() => {
         fetch("https://dvonb.xyz/api/2025-fall/itis-3135/students?full=1")
             .then((response) => {
@@ -24,8 +31,8 @@ export default function ClassIntroduction() {
             .catch((error) => setError(error.message));
     }, [])
 
+    {/* Search filtering*/ }
     const filteredIntroductionData = introductionData.filter((data) => {
-        {/* Search filtering*/ }
         if (nameSearch === "") return true;
         const fullStudentName = `${data.name.first} ${data.name.middleInitial}. "${data.name.preferred}" ${data.name.last}`;
         return fullStudentName.toLowerCase().includes(nameSearch.toLowerCase());
@@ -36,9 +43,8 @@ export default function ClassIntroduction() {
 
             {/*Search */}
             <label>
-                Search for Student: &nbsp;&nbsp;
+                Search for Student:
                 <input type="text" onChange={(event) => setNameSearch(event.target.value)} />
-                name search: {nameSearch}
             </label>
 
             {/*Check Box Filters */}
@@ -48,61 +54,102 @@ export default function ClassIntroduction() {
                 <label>
                     Name:
                     <input type="checkbox" checked={displayName} onClick={() => setDisplayName(!displayName)} />
-                    display name: {displayName.toString()}
                 </label>
 
-                <br />
+
 
                 <label>
                     Mascot:
                     <input type="checkbox" checked={displayMascot} onClick={() => setDisplayMascot(!displayMascot)} />
-                    display Mascot: {displayMascot.toString()}
                 </label>
 
-                <br />
+
 
                 <label>
                     Image:
                     <input type="checkbox" checked={displayImage} onClick={() => setDisplayImage(!displayImage)} />
-                    display Image: {displayImage.toString()}
+                </label>
+
+
+                <label>
+                    Personal Statement:
+                    <input type="checkbox" checked={displayPersonalStatement} onClick={() => setDisplayPersonalStatement(!displayPersonalStatement)} />
+                </label>
+
+
+                <label>
+                    Backgrounds:
+                    <input type="checkbox" checked={displayBackgrounds} onClick={() => setDisplayBackgrounds(!displayBackgrounds)} />
+                </label>
+
+
+                <label>
+                    Classes:
+                    <input type="checkbox" checked={displayClasses} onClick={() => setDisplayClasses(!displayClasses)} />
+                </label>
+
+
+                <label>
+                    Extra Information:
+                    <input type="checkbox" checked={displayExtraInformation} onClick={() => setDisplayExtraInformation(!displayExtraInformation)} />
+                </label>
+
+
+                <label>
+                    Quote:
+                    <input type="checkbox" checked={displayQuote} onClick={() => setDisplayQuote(!displayQuote)} />
+                </label>
+
+
+                <label>
+                    Links:
+                    <input type="checkbox" checked={displayLinks} onClick={() => setDisplayLinks(!displayLinks)} />
                 </label>
             </section>
 
-            {/*Slide Show */}
-            <button onClick={() => setShowSlideShow(!showSlideShow)}>
-                {showSlideShow ? "View All" : "View Slideshow"}
-            </button>
-            <br />
-            {error == null ? "" : <p>Error Code: {error}</p>} {/* Error Message*/}
+            {/*Slide Show button*/}
+            <section>
+                <button onClick={() => setShowSlideShow(!showSlideShow)}>
+                    {showSlideShow ? "View All" : "View Slideshow"}
+                </button>
+            </section>
+
+
+
+
+            {/* Error Message*/}
+            {error == null ? "" : <p>Error Code: {error}</p>}
             {
 
                 showSlideShow
-                    ? //if true
+                    ? //if true display in a slide show
                     <>
+                        {/* Slide Show buttons*/}
+                        <button onClick={() => indexOfSlideShow - 1 < 0 ? setIndexOfSlideShow(filteredIntroductionData.length - 1) : setIndexOfSlideShow(indexOfSlideShow - 1)}>Previous</button>
+                        <button onClick={() => indexOfSlideShow + 1 >= filteredIntroductionData.length ? setIndexOfSlideShow(0) : setIndexOfSlideShow(indexOfSlideShow + 1)}>Next</button>
 
-                        <button onClick={() => indexOfSlideShow - 1 < 0 ? setIndexOfSlideShow(filteredIntroductionData.length -1) : setIndexOfSlideShow(indexOfSlideShow - 1)}>Previous</button>
-                        <button onClick={() => indexOfSlideShow + 1 >= filteredIntroductionData.length ? setIndexOfSlideShow(0) : setIndexOfSlideShow(indexOfSlideShow + 1)}>Next</button>  
-                        
                         {/* Slider */}
-                        <input 
-                        id= {"slider"} 
-                        type="range" 
-                        max = {filteredIntroductionData.length -1}
-                        min ={0}
-                        step= {1}
-                        onChange={(event) =>
-                            setIndexOfSlideShow(event.target.value)
-                        }
-                        value = {indexOfSlideShow}
+                        <input
+                            id={"slider"}
+                            type="range"
+                            max={filteredIntroductionData.length - 1}
+                            min={0}
+                            step={1}
+                            onChange={(event) =>
+                                setIndexOfSlideShow(event.target.value)
+                            }
+                            value={indexOfSlideShow}
                         />
-                        
-                        <StudentIntroduction studentData={filteredIntroductionData[indexOfSlideShow]} displayMascot={displayMascot} displayName={displayName} displayImage={displayImage}></StudentIntroduction>
+
+                      
+                            <StudentIntroduction studentData={filteredIntroductionData[indexOfSlideShow]} displayMascot={displayMascot} displayName={displayName} displayImage={displayImage} displayPersonalStatement={displayPersonalStatement} displayBackgrounds={displayBackgrounds} displayClasses={displayClasses} displayExtraInformation={displayExtraInformation} displayQuote={displayQuote} displayLinks={displayLinks}></StudentIntroduction>
+                      
+
                     </>
 
-                    : // if false
-
+                    : // if false display information normally
                     filteredIntroductionData.map((studentData, index) =>
-                        <StudentIntroduction key={index} studentData={studentData} displayMascot={displayMascot} displayName={displayName} displayImage={displayImage}></StudentIntroduction>
+                        <StudentIntroduction key={index} studentData={studentData} displayMascot={displayMascot} displayName={displayName} displayImage={displayImage} displayPersonalStatement={displayPersonalStatement} displayBackgrounds={displayBackgrounds} displayClasses={displayClasses} displayExtraInformation={displayExtraInformation} displayQuote={displayQuote} displayLinks={displayLinks}></StudentIntroduction>
                     )
             }
             {/*<p> {JSON.stringify(introductionData)}</p>*/} {/* Display JSON studentData*/}
--- src/StudentIntroduction.jsx
+++ src/StudentIntroduction.jsx
@@ -1,5 +1,4 @@
-
-export default function StudentIntroduction({studentData, displayName, displayMascot, displayImage }) {
+export default function StudentIntroduction({ studentData, displayName, displayMascot, displayImage, displayPersonalStatement, displayBackgrounds, displayClasses, displayExtraInformation, displayQuote, displayLinks }) {
 
     return (
         <article>
@@ -31,44 +30,68 @@ export default function StudentIntroduction({studentData, displayName, displayMa
             }
 
 
-            <p>{studentData.personalStatement}</p>
+            <p>{displayPersonalStatement && studentData.personalStatement}</p>
+
+
             <ul>
-                <li><strong>Personal Background</strong>: {studentData.backgrounds.personal}</li>
-                <li><strong>Professional Background</strong>: {studentData.backgrounds.professional}</li>
-                <li><strong>Academic Background</strong>: {studentData.backgrounds.academic}</li>
-                <li><strong>Subject Background</strong>: {studentData.backgrounds.subject}</li>
-                <li><strong>Primary Computer</strong>: {studentData.platform.device}, {studentData.platform.os}</li>
-                <li><strong>Courses</strong>:
-                    <ol>
-                        {
-                            studentData.courses.map((course, index) =>
-                                <li key={index}>
-                                    <strong>{course.dept} {course.num} - {course.name}</strong> :  {course.reason}
-                                </li>
-                            )
-                        }
-                    </ol>
-                </li>
-                <li>
-                    <strong>Fun Fact: </strong> {studentData.funFact}
-                </li>
-                <li>
-                    <strong>Additional Information: </strong> {studentData.additional}
-                </li>
+                {
+                    displayBackgrounds &&
+                    <>
+                        <li><strong>Personal Background</strong>: {studentData.backgrounds.personal}</li>
+                        <li><strong>Professional Background</strong>: {studentData.backgrounds.professional}</li>
+                        <li><strong>Academic Background</strong>: {studentData.backgrounds.academic}</li>
+                        <li><strong>Subject Background</strong>: {studentData.backgrounds.subject}</li>
+                    </>
+                }
+                {
+                    displayClasses &&
+                    <li><strong>Courses</strong>:
+                        <ol>
+                            {
+                                studentData.courses.map((course, index) =>
+                                    <li key={index}>
+                                        <strong>{course.dept} {course.num} - {course.name}</strong> :  {course.reason}
+                                    </li>
+                                )
+                            }
+                        </ol>
+                    </li>
+                }
+
+                {
+                    displayExtraInformation &&
+                    <>
+                        <li><strong>Primary Computer</strong>: {studentData.platform.device}, {studentData.platform.os}</li>
+                        <li>
+                            <strong>Fun Fact: </strong> {studentData.funFact}
+                        </li>
+                        <li>
+                            <strong>Additional Information: </strong> {studentData.additional}
+                        </li>
+                    </>
+                }
+
             </ul>
-            <p>
-                <p id="quote"><em > "{studentData.quote.text}" </em></p>
-                <p id="quote-name">-{studentData.quote.author}</p>
-            </p>
-            <nav>
-                <a href={studentData.links.charlotte}>CLT Web</a> {" " + studentData.divider + " "}
-                <a href={studentData.links.github}>GitHub</a> {" " + studentData.divider + " "}
-                <a href={studentData.links.githubio}>GitHub.io</a> {" " + studentData.divider + " "}
-                <a href={studentData.links.itis3135}>ITIS 3135</a> {" " + studentData.divider + " "}
-                <a href={studentData.links.freecodecamp}>freeCodeCamp</a> {" " + studentData.divider + " "}
-                <a href={studentData.links.codecademy}>Code Academy</a> {" " + studentData.divider + " "}
-                <a href={studentData.links.linkedin}>LinkedIn</a>
-            </nav>
+            {
+                displayQuote &&
+                <p>
+                    <p id="quote"><em > "{studentData.quote.text}" </em></p>
+                    <p id="quote-name">-{studentData.quote.author}</p>
+                </p>
+            }
+            {
+                displayLinks &&
+                <nav>
+                    <a href={studentData.links.charlotte}>CLT Web</a> {" " + studentData.divider + " "}
+                    <a href={studentData.links.github}>GitHub</a> {" " + studentData.divider + " "}
+                    <a href={studentData.links.githubio}>GitHub.io</a> {" " + studentData.divider + " "}
+                    <a href={studentData.links.itis3135}>ITIS 3135</a> {" " + studentData.divider + " "}
+                    <a href={studentData.links.freecodecamp}>freeCodeCamp</a> {" " + studentData.divider + " "}
+                    <a href={studentData.links.codecademy}>Code Academy</a> {" " + studentData.divider + " "}
+                    <a href={studentData.links.linkedin}>LinkedIn</a>
+                </nav>
+            }
+
 
             <hr />
         </article>
